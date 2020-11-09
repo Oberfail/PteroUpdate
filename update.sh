b78b72e827c8f4e1ec227a92e050d5605a14976c
Oberfail <ich.bin.daniel.9@googlemail.com>
2020-11-09 09:10:00 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo ""
+echo "Check out https://pterodactyl.io for the official way to update"
+echo "Its not reccomended to use auto update scripts, they can be outdated or even break stuff."
+echo "The latest Release of this Shell Script can be found on https://github.com/Oberfail/PteroUpdater"
+read -p "Do you wish to Continue (y/n)" -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 
+fi
+
 #Pterodactyl Panel update to latest
 cd /var/www/pterodactyl
 php artisan down
@@ -18,6 +28,7 @@ php artisan db:seed --force
 chown -R www-data:www-data * /var/www/pterodactyl
 php artisan up
 php artisan queue:restart
+echo "Your Panel has successfully been updated to the latest version"
 
 #Pterodactyl Wings update to latest
 cd /usr/local/bin
@@ -28,3 +39,4 @@ DOWNLOAD_URL=$(curl https://api.github.com/repos/pterodactyl/wings/releases/late
 curl -L --create-dirs -o /usr/local/bin/wings "$DOWNLOAD_URL"
 chmod u+x /usr/local/bin/wings
 systemctl restart wings
+echo "Your Wings have successfully been updated to the latest version and have been restarted"
